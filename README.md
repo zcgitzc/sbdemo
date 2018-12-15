2442a2c14b261491010ae99c7544d7136d94cdf8
zark <486289410@qq.com>
2018-12-16 01:26:27 +0800

    项目启动成功

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sbdemo
+### markdown 语法学习转到：https://www.jianshu.com/p/191d1e21f7ed
+# sbdemo项目
 springboot 学习
+
+# 搭建Mybatis环境注意点：
+1.@MapperScan 扫描Mybatis注解
+> @MapperScan(basePackages = "com.zark.sbproject.boot.entity.**.mapper", annotationClass = org.springframework.stereotype.Repository.class)
+
+2.application里面配置扫描xml文件的路劲
+> mybatis.mapper-locations=classpath*:com/**/mapper/*Mapper*.xml
--- boot-web/src/main/java/com/zark/sbproject/boot/web/controller/App.java
+++ boot-web/src/main/java/com/zark/sbproject/boot/web/controller/App.java
@@ -1,11 +1,14 @@
 package com.zark.sbproject.boot.web.controller;
 
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//扫描Spring注解 包括不限于：@Controller @Service
+@ComponentScan({ "com.zark.sbproject.boot" })
+@MapperScan(basePackages = "com.zark.sbproject.boot.entity.**.mapper", annotationClass = org.springframework.stereotype.Repository.class)
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
